a43afc81ee00cda0dced3bd7eea3676dd023a72d
Teodoro Obando <teo.obando09@gmail.com>
2022-07-22 23:20:35 -0600

    [ADD] translation with a script in the same document

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
-alert('Hello world, this is a simple js test, please click on accept to continue')
+let text = HTMLElement
+
+window.onload = function(){
+    text = document.getElementById('test')
+    text.innerText = `Ingeniero en Sistemas, especializado en el desarrollo web
+    Full Stack; usando diversas tecnologías tales como HTML, CSS, JavaScript, 
+    Vue.js, React.js, Bootstrap y Tailwindcss para frontend;
+    y PHP (Laravel), Python (Django), Node.js (Next.js) para backend.`
+}
+
+function spanish(){
+    text = document.getElementById('test')
+    text.innerText = `Ingeniero en Sistemas, especializado en el desarrollo web
+    Full Stack; usando diversas tecnologías tales como HTML, CSS, JavaScript, 
+    Vue.js, React.js, Bootstrap y Tailwindcss para frontend;
+    y PHP (Laravel), Python (Django), Node.js (Next.js) para backend.`
+}
+
+function english(){
+    text = document.getElementById('test')
+    text.innerText = `A System Engineer who works as a full stack web developer,
+    whith some tecnologies like HTML, CSS, JavaScript, 
+    Vue.js, React.js, Bootstrap and Tailwindcss to the frontend.
+    PHP (Laravel), Python (Django) and Node.js (Next.js) to the 
+    backend.`
+}
